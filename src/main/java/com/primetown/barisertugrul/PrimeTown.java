70d27af17e32af042e0280c9007b87bcd32fa997
barisertu <bertugrul@tsn.at>
2020-05-20 14:25:48 +0200

    Project Application name changed from BarisertugrulApplication to PrimeTown

--- src/main/java/com/primetown/barisertugrul/PrimeTown.java
+++ src/main/java/com/primetown/barisertugrul/PrimeTown.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BarisertugrulApplication {
+public class PrimeTown {
 
     public static void main(String[] args) {
-        SpringApplication.run(BarisertugrulApplication.class, args);
+        SpringApplication.run(PrimeTown.class, args);
     }
 
 }
